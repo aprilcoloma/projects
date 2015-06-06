b30949956fa5884a2367c9742296616d48e7cc4a
April Coloma <april.coloma@ninthdesign.com>
2015-06-06 19:15:47 +0800

    delete the days label array from the js code because these are all hard coded in our html file

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -2,7 +2,6 @@
 var doc              = document,
 	calDate 	 	 = new Date(),
 	monthLabels      = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-	dayName          = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
 	currentMonth     = calDate.getMonth(), // 0 - 11 (5)
 	year             = calDate.getFullYear(), // 2015	 
 	firstDate        = monthLabels[currentMonth] + " " + 1 + " " + year, // June 1 2015,
@@ -22,17 +21,18 @@ var doc              = document,
 		calendarHeader.innerHTML =  monthLabels[currentMonth] + " " + year;
 
 
-		// create first row
-
 		var count = 1;
 
+		// create rows for the weeks (approximately 6 rows)
 		for (var i = 0; i < 6; i++) {
+
 			var tr = doc.createElement('tr');
 
-
+			// create cells for the days
 			for (var j = 0; j < 7; j++) {
 				var td = doc.createElement('td');
 
+				// if days are less than our total days and our rows are greater than zero, or cell is greater that our current day
 				if ( count <= totalDays && (i > 0 || j >= dayIndex) ) {
 					td.innerHTML = count;
 					count++;	
@@ -42,67 +42,12 @@ var doc              = document,
 				
 			}
 
+			// display the days
 			tableBody.appendChild(tr);
 		}
 		
 
-
-		// var count = 1;
-
-		
-		// for ( var d = 0; d <= 6; d++) {
-
-		// 	if ( d === dayIndex ) {
-		// 		break;
-		// 	}
-
-		// 	var td = doc.createElement('td');
-		// 	td.innerHTML = "blank";
-		// 	tr.appendChild(td);
-		// }
-
-		// tableBody.appendChild(tr);
-
-
-
-		// var count = 1;
-		// for (var e = 0; e <= 6; e++) {
-		// 	var td1 = doc.createElement('td');
-		// 	td1.innerHTML = count;
-		// 	count++;
-		// 	tr.appendChild(td1);
-		// }
-
-		// tableBody.appendChild(tr);
-		// // create 2nd row
-		
-
-		// // rest of the rows.
-		// for (var r = 3; r <= 6; r++) {
-		// 	tr = doc.createElement('tr');
-
-		// 	for ( var c = 0; c<=6; c++) {
-		// 		if (count > totalDays) {
-		// 			tableBody.appendChild(tr);
-		// 			return tableBody;
-		// 		}
-
-		// 		var td = doc.createElement('td');
-
-		// 		td.innerHTML = count;
-		// 		count++;
-		// 		tr.appendChild(td);
-
-		// 	}
-
-		// 	tableBody.appendChild(tr);
-		// }
-		
-
-
-
-
-
+	
 	};
 
 	getCalendar();
