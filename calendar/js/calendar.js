e06bb61465444b12260199a5667f26553deb0442
April Coloma <april.coloma@ninthdesign.com>
2015-06-06 12:53:02 +0800

    create .gitignore and add calendar directory

---
+++ calendar/js/calendar.js
@@ -0,0 +1,4 @@
+var labels       = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	months       = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
