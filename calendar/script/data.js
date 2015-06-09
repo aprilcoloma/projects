5cd5c2025c74bedf7b44197bed97baeca9a45ecc
April Coloma <april.coloma@ninthdesign.com>
2015-06-10 07:51:01 +0800

    add few more events to our list

--- calendar/script/data.js
+++ calendar/script/data.js
@@ -10,9 +10,26 @@ var eventList = [
     {
         'date': 'June 12, 2015',
         'events': [
-            'Go to the bank',
-            'Buy new phone',
-            'Buy new pencils and other art materials'
+            'Go to Paris',
+            'Buy souvenirs'
+        ]
+    },
+    {
+        'date': 'June 30, 2015',
+        'events': [
+            'Sleep all day'
+        ]
+    },
+    {
+        'date': 'July 16, 2015',
+        'events': [
+            'Buy birthday gift'
+        ]
+    },
+    {
+        'date': 'November 1, 2015',
+        'events': [
+            'Attend a halloween party'
         ]
     }
 ];
