be9bc078bc2e99faecef398264000d7c7c4a2ea3
April Coloma <april.coloma@ninthdesign.com>
2015-06-09 14:55:04 +0800

    add data.js file

---
+++ calendar/script/data.js
@@ -0,0 +1,10 @@
+var eventList = [
+    {
+        'date': 'June 20, 2015',
+        'events': [
+            'Go to the bank',
+            'Buy new phone',
+            'Buy new pencils and other art materials'
+        ]
+    }
+];
