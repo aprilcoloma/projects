3abe3d05296c7dde115a9b1f8930387824689df8
April Coloma <april.coloma@ninthdesign.com>
2015-06-10 10:55:31 +0800

    add event title and event time to the code

--- calendar/script/data.js
+++ calendar/script/data.js
@@ -2,34 +2,58 @@ var eventList = [
     {
         'date': 'June 9, 2015',
         'events': [
-            'Go to the bank',
-            'Buy new phone',
-            'Buy new pencils and other art materials'
+            {
+                'title': 'Go to the bank',
+                'description': 'Go to the bank to withdraw',
+                'time': '10:00 am'
+            },
+            {
+                'title': 'Buy new phone',
+                'description': 'Buy a new phone: iPhone or Samsung',
+                'time': '10:30 am'
+            },
+            {
+                'title': 'Buy new pencils and other art materials',
+                'description': 'Buy new pencils and other art materials for a project',
+                'time': '11:00 am'
+            }
         ]
+
+
     },
     {
         'date': 'June 12, 2015',
         'events': [
-            'Go to Paris',
-            'Buy souvenirs'
+            {
+                'title': 'Meeting with the bosses',
+                'description': 'Meeting with the bosses for a new project',
+                'time': '08:00 am'
+            }
         ]
     },
     {
-        'date': 'June 30, 2015',
+        'date': 'July 10, 2015',
         'events': [
-            'Sleep all day'
-        ]
-    },
-    {
-        'date': 'July 16, 2015',
-        'events': [
-            'Buy birthday gift'
+            {
+                'title': 'Shopping',
+                'description': 'Buy new clothes',
+                'time': '09:00 am'
+            },
+            {
+                'title': 'Take a nap',
+                'description': 'Take a nap for an hour',
+                'time': '10:00 am'
+            }
         ]
     },
     {
         'date': 'November 1, 2015',
         'events': [
-            'Attend a halloween party'
+            {
+                'title': 'Attend a party',
+                'description': 'Attend a holloween party',
+                'time': '10:00 pm'
+            }
         ]
-    }
+    },
 ];
