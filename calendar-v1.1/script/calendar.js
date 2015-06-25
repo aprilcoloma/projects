96a43057a19bec914f51e6e1b59db7778d3a0b32
April Coloma <april.coloma@ninthdesign.com>
2015-06-26 07:34:11 +0800

    add calendar with event range features

---
+++ calendar-v1.1/script/calendar.js
@@ -0,0 +1,240 @@
+var doc             = document,
+    calDate         = new Date(),
+    calCounter      = 0, 
+
+    currentMonth, firstDate, year, firstDayOfMonth, firstDay, dayIndex, totalDays, calNavLink, eventList, eventDate, events, calendarDate, eventListItem, 
+    eventListDateStart, eventListDateEnd, td, count, today, dd, mm, yyyy, c, yr, mt, dy, newConvertedDate,
+
+    monthLabels     = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    dayName         = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    dateToday       = doc.getElementById('date-today'),
+    eventWrapper    = doc.getElementById('event-wrapper'),
+    tableBody       = doc.getElementById('tbl-body'),
+    calendarHeader  = doc.getElementById( 'month-year' );
+
+
+var createCalendar = function() {
+    currentMonth     = calDate.getMonth() + calCounter; // 0 - 11 (5)
+    year             = calDate.getFullYear(); // 2015
+
+    firstDate        = monthLabels[currentMonth] + " " + 1 + " " + year; // June 1 2015 
+    firstDayOfMonth  = new Date(firstDate).toDateString(); // Mon Jun 01 2015,
+    firstDay         = firstDayOfMonth.substring(0,3); // Mon
+    dayIndex         = dayName.indexOf(firstDay); // 1 = Mon
+    totalDays        = new Date(year, currentMonth+1, 0).getDate() + "";
+    calNavLink       = '';
+
+    // display the month and the year
+    calendarHeader.innerHTML =  monthLabels[currentMonth] + " " + year;
+
+    // days counter (to be compared against the total days)
+    count = 1;
+
+    // create rows for the weeks (approximately 6 rows)
+    var i = 0;
+    for (i; i < 6; i++) {
+
+        var tr = doc.createElement('tr');
+
+        // create cells for the days
+        var j = 0;
+        for (j; j < 7; j++) { 
+            td = doc.createElement('td');
+
+            // if days are less than our total days and our rows are greater than zero, or cell is greater that our current day, 
+            // (start displaying on the first day of the month, cells before the first day should be left blank)
+            if ( count <= totalDays && (i > 0 || j >= dayIndex) ) {
+                var dateWrapper = doc.createElement( 'span' );
+
+                today = new Date();
+                dd = today.getDate() + "";
+                mm = today.getMonth();
+                yyyy = today.getFullYear();
+
+                dateWrapper.classList.add('day');
+                dateWrapper.innerHTML = count;
+                td.appendChild(dateWrapper);
+
+                // loop our events
+                var k = 0;
+                for (; k < eventList.length; k++ ) {
+                    eventListItem = eventList[k];
+                    eventListDateStart = eventList[k].dateStart;
+                    eventListDateEnd = eventList[k].dateEnd;
+
+                    if ( eventListDateEnd) {
+                        eventListDateEnd = eventListDateEnd;
+                    } else {
+                        eventListDateEnd = "";
+                    }
+
+                    var strippedDateStart = dateTools.dateStripper(eventListDateStart),
+                        strippedDateEnd = dateTools.dateStripper(eventListDateEnd),
+                        strippedTime = dateTools.timeStripper(eventListDateStart),
+                        eventFullYear = new Date(strippedDateStart).getFullYear(),
+                        eventDay = new Date(strippedDateStart).getDate(),
+                        eventMonth = monthLabels[new Date(strippedDateStart).getMonth()],
+                        eventDayEnd = new Date(strippedDateEnd).getDate();
+
+                    var monthStringify = (currentMonth + 1) + "";
+
+                    if ( monthStringify.length < 2 ) {
+                        monthStringify = "0" + monthStringify;
+                    }
+
+                    var calendarDate = year + "-" + monthStringify + "-" + count;
+
+                    if ( calendarDate === strippedDateStart || calendarDate === strippedDateEnd ) {
+                        td.classList.add('with-event');
+
+                        td.setAttribute('date-start', strippedDateStart);
+
+                        if ( eventListDateEnd) {
+                            var eventRange = new Date(strippedDateEnd).getDate() - eventDay;
+                            td.setAttribute('date-end', strippedDateEnd);
+                            td.setAttribute('day-range', eventRange);
+                            
+                        } else {
+                            eventListDateEnd = "";
+                        }
+
+                        
+                    }
+
+                    if ( count > eventDay && count < eventDayEnd ) {
+                        td.classList.add('with-event');
+                    }
+
+
+
+                }
+
+                if ( td.className === 'with-event' ) {
+                    var eventIndicator = doc.createElement('span');
+
+                    eventIndicator.classList.add('event-indicator');
+                    td.insertBefore( eventIndicator, dateWrapper.nextSibling );
+                }
+
+                // add days
+                count++;
+
+                // mark today
+                if ( dateWrapper.innerHTML === dd && mm === currentMonth && yyyy === year ) {
+                    td.classList.add('today');
+                }
+
+
+            } else {
+                td.innerHTML = '&nbsp';
+                td.classList.add('empty-cell');
+            }
+
+            // add the rows to our table body
+            tr.appendChild(td);
+        }
+
+        // display the days
+        tableBody.appendChild(tr);
+    }
+};
+
+
+var dateTools = {
+    dateStripper: function(dt) {
+        dt = dt.substring(0, 10);
+        return dt;
+    },
+    timeStripper: function(t) {
+        t = t.substring(11);
+        return t;
+    },
+    dayStripper: function(dy) {
+        dy = dy.substring(5,7);
+        return dy;
+    }
+};
+
+
+// next or previous calendar
+var changeCalendar = {  
+
+    showNextCalendar: function() {
+        calCounter++; // go to the next month contents
+
+        if ( currentMonth+1 > monthLabels.length - 1 ) {
+            this.resetCalendar();
+        } else {
+            currentMonth++;
+            createCalendar(); // create contents again
+        }
+    },
+
+    showPreviousCalendar: function() {
+        calCounter--; // go to the previous month contents
+        currentMonth--;
+
+        if ( currentMonth < 0 ) {
+            currentMonth = monthLabels.length-1;
+            this.setToPreviousCalendar();
+        } else {
+            createCalendar(); // create contents again  
+        }
+
+        
+    },
+
+    resetCalendar: function() {
+
+        // add a year to our current year
+        calDate = new Date(year+1, 0);
+
+        // reset our calendar counter
+        calCounter = 0;
+
+        tableBody.innerHTML = null; // empty the table
+        createCalendar();
+    },
+
+    setToPreviousCalendar: function() {
+
+        // substract a year from our current year, and set the month to december
+        calDate = new Date(year-1, 11);
+
+        // reset our calendar counter
+        calCounter = 0;
+        tableBody.innerHTML = null; // empty the table
+        createCalendar();
+    }
+
+};
+
+
+
+
+// initialize the calendar
+window.addEventListener('load', function() {
+
+    // display the current month
+    createCalendar();
+
+    // calendar navigation
+    var calNav = doc.getElementById('cal-navigation'),
+        prevNav = doc.getElementById('prev-nav'),
+        nextNav = doc.getElementById('next-nav');
+
+    calNav.addEventListener('click', function(e) {
+
+        calNavLink = e.target.id;
+
+        tableBody.innerHTML = null; // empty the table
+
+        if ( calNavLink === 'next-nav' ) {
+            changeCalendar.showNextCalendar();
+        } else {
+            changeCalendar.showPreviousCalendar();
+        }
+
+    }, false);
+
+});
---
+++ calendar-v1.1/script/data.js
@@ -0,0 +1,41 @@
+var eventList = [
+    {
+        "title": "Paris Vacation!!",
+        "description": "Paris Vacation!!",
+        "dateStart": "2015-06-10 06:00:00",
+        "dateEnd": "2015-06-20 9:40:00"
+    },
+    {
+        "title": "Attend Entrepreneurship Workshop",
+        "description": "Attend Entrepreneurship Workshop",
+        "dateStart": "2015-06-22 08:00:00",
+        "dateEnd": "2015-06-23 10:00:00"
+    },
+    {
+        "title": "Join a Relief Donation",
+        "description": "Join a Relief Donation",
+        "dateStart": "2015-06-24 15:30:00"
+    },
+    {
+        "title": "Go sky diving",
+        "description": "Go sky diving",
+        "dateStart": "2015-06-29 08:00:00"
+    },
+    {
+        "title": "Read lots of Javascript e-books!!",
+        "description": "Read lots of Javascript e-books!!",
+        "dateStart": "2015-07-11 09:00:00",
+        "dateEnd": "2015-07-12 10:00:00"
+    },
+    {
+        "title": "Go to Davao for Kadayawan sa Dabaw",
+        "description": "Go to Davao for Kadayawan sa Dabaw",
+        "dateStart": "2015-08-15 04:00:00",
+        "dateEnd": "2015-08-19 15:00:00"
+    },
+    {
+        "title": "Go to bookstore",
+        "description": "Go to bookstore",
+        "dateStart": "2015-08-19 11:00:00"
+    }
+];
